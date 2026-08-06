64b896569e69723060618f7258213e7b28ec2c1c
Arun <arunmohanarunz06@gmail.com>
2026-08-06 23:58:33 +0530

    propertysearch css to vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { bunny } from 'laravel-vite-plugin/fonts';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css',        'resources/css/vendor/home.css',
+            input: ['resources/css/app.css','resources/css/vendor/home.css','resources/css/vendor/property-search.css',
                 'resources/js/app.js'],
             refresh: true,
             fonts: [
