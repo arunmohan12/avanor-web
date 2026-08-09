3ea41077b36397e85840314f8a057cd74454460f
Arun <arunmohanarunz06@gmail.com>
2026-08-09 17:21:36 +0530

    contact css

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,11 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css',
                 'resources/css/vendor/home.css',
+                'resources/css/vendor/contact.css',
                 'resources/js/pages/home.js',
                 'resources/css/vendor/property-search.css',
                 'resources/css/vendor/about.css',
+                
                 'resources/js/pages/contact.js',
                 
                 'resources/js/app.js'],
